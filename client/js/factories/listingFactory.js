2792483767b4cb58d1563b6be18e736b46eb1b0d
JoshuaSwerdlow <35569274+JoshSwerdlow@users.noreply.github.com>
2019-02-12 19:27:40 -0500

    Update listingFactory.js

--- client/js/factories/listingFactory.js
+++ client/js/factories/listingFactory.js
@@ -1,18 +1,18 @@
 angular.module('listings', []).factory('Listings', function($http) {
   var methods = {
     getAll: function() {
-      return $http.get('http://localhost:8080/api/listings');
+      return $http.get('https://theap.herokuapp.com/api/listings');
     },
-	
+
 	create: function(listing) {
-	  return $http.post('http://localhost:8080/api/listings', listing);
-    }, 
+	  return $http.post('https://theap.herokuapp.com/api/listings', listing);
+    },
 
     delete: function(id) {
 	   /**TODO
         return result of HTTP delete method
        */
-
+       return $http.delete('https://theap.herokuapp.com/api/listings', id);
     }
   };
 
